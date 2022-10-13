ea67e2633d8b114f34397c0d14486886b28c7b99
Bruno Reis <85531625+brunocbreis@users.noreply.github.com>
2022-10-12 22:12:43 -0300

    Fix: reordered card definition

--- card.go
+++ card.go
@@ -5,16 +5,16 @@ import (
 	"strconv"
 )
 
-// Define a card
-type card struct {
-	Value int
-	Suit  string
+// Define a Card
+type Card struct {
+	Value int    `json:"value"`
+	Suit  string `json:"suit"`
 }
 
 var cardValues = [13]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}
 var cardSuits = [4]string{"Hearts", "Spades", "Diamonds", "Clubs"}
 
-func (c card) name() string {
+func (c Card) name() string {
 	// Returns a human-readable name for a card.
 
 	letterCards := map[int]string{1: "Ace", 11: "Jack", 12: "Queen", 13: "King"}
