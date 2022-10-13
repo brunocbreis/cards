3fb975b27e61a112b5dd20cb8920784c5424dfa8
Bruno Reis <85531625+brunocbreis@users.noreply.github.com>
2022-10-12 22:13:50 -0300

    Feat: added funcs to write to / read from JSON

--- deck.go
+++ deck.go
@@ -1,21 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
-type deck []card
+type Deck []Card
 
-func newDeck() deck {
-	var d deck
+func newDeck() Deck {
+	var d Deck
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
-			d = append(d, card{value, suit})
+			d = append(d, Card{value, suit})
 		}
 	}
 	return d
 }
 
-func (d deck) print() {
+func (d Deck) print() {
 	for _, card := range d {
 		fmt.Println(card.name())
 	}
+}
+
+func (d Deck) deal(handSize int) (Deck, Deck) {
+	// Deals a hand and returns the hand and the new deck back
+
+	hand := d[:handSize]
+	deck := d[handSize:]
+
+	return hand, deck
+}
+
+func (d Deck) toString() string {
+	var deckSlice []string
+	for _, card := range d {
+		deckSlice = append(deckSlice, card.name())
+	}
+
+	return strings.Join(deckSlice, ", ")
+}
+
+func (d Deck) toJSON(fileName string) error {
+	jsonBytes, err := json.MarshalIndent(d, "", "\t")
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.WriteFile(fileName, jsonBytes, 0666)
+	return err
+}
+
+func newDeckFromJSON(fileName string) (Deck, error) {
+	jsonBytes, err := os.ReadFile(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var newDeck Deck
+	json.Unmarshal(jsonBytes, &newDeck)
+
+	return newDeck, err
 }
--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
 func main() {
-	deck := newDeck()
+	// deck := newDeck()
 
-	deck.print()
+	// var hand Deck
+
+	// hand, deck = deck.deal(5)
+
+	// fmt.Println("Your hand is:")
+	// hand.print()
+	// fmt.Printf("\nThere are %d cards left in the deck:\n", len(deck))
+	// fmt.Println(deck.toString())
+
+	// hand.toJSON("myHand.json")
+
+	importedHand, _ := newDeckFromJSON("myHand.json")
+	importedHand.print()
+
 }
